713872541af2b377790c9bf13871fff3cb1ae0ee
danielpipa <danielpipa@gmail.com>
2026-07-08 11:35:10 -0300

    First really version.

--- fd_vs_fem.py
+++ fd_vs_fem.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as ss
-from scipy.integrate import quad, simpson
+from scipy.integrate import quad, simpson, cumulative_simpson
 import itertools
 from tqdm import tqdm
 import sympy as sp # import symbols, Piecewise, integrate, And
@@ -10,7 +10,7 @@ c = [3000, 1500]
 dt = 2e-9
 dx = 20e-6
 Lx = 5e-3
-Lt = 2e-6
+Lt = 3e-6
 Nx = round(Lx / dx)
 Nt = round(Lt / dt)
 
@@ -39,24 +39,8 @@ def ricker(t, f0):
     tmp = (1 - (t/sigma)**2) * np.exp(-t**2 / (2 * sigma**2))
     return tmp / np.max(tmp)
 s = ricker(t - t0, f0)
-# plt.plot(t, s, t, s2)
+# plt.plot(t, s)
 # plt.show(block=True)
-
-# def phi(x0, xi, i):
-#     hb = xi[i] - xi[i-1] if i > 0 else -np.inf
-#     hf = xi[i+1] - xi[i] if i < N - 1 else -np.inf
-#     x = x0 - xi[i]
-#     conds = [(-hb <= x) & (x <= 0), (0 <= x) & (x <= hf)]
-#     funs = [lambda x: x/hb + 1, lambda x: 1 - x/hf, 0]
-#     return np.piecewise(x, conds, funs)
-#
-# def dphi(x0, xi, i):
-#     hb = xi[i] - xi[i-1] if i > 0 else -np.inf
-#     hf = xi[i+1] - xi[i] if i < N - 1 else -np.inf
-#     x = x0 - xi[i]
-#     conds = [(-hb <= x) & (x <= 0), (0 <= x) & (x <= hf)]
-#     funs = [lambda x: 1/hb, lambda x: -1/hf, 0]
-#     return np.piecewise(x, conds, funs)
 
 xsp = sp.Symbol("xsp")
 xspim1, xspi, xspip1 = sp.symbols("xspim1, xspi, xspip1")
@@ -105,31 +89,21 @@ def buildMat(x, phi_i, phi_j):
         M[i, j] = Mij(xim1, xi, xip1, xjm1, xj, xjp1)
     return M
 
-# def buildK(x):
-#     K = np.zeros((N, N))
-#     for i, j in tqdm(itertools.product(range(N), range(N))):
-#         xim1 = x[i - 1] if i > 0 else np.inf
-#         xip1 = x[i + 1] if i < N - 1 else -np.inf
-#         xi = x[i]
-#         xjm1 = x[j - 1] if j > 0 else np.inf
-#         xjp1 = x[j + 1] if j < N - 1 else -np.inf
-#         xj = x[j]
-#         dphi_i = dphi_i.subs({xspim1: xim1, xspi: xi, xspip1: xip1})
-#         dphi_j = dphi_i.subs({xspim1: xjm1, xspi: xj, xspip1: xjp1})
-#         K[i, j] = sp.integrate(dphi_i * dphi_j, (xsp, x[0], x[-1]))
-#     return K
-
 def buildf(x, k):
     fi = sp.lambdify((xspim1, xspi, xspip1), sp.integrate(f_i * phi_i, (xsp, x[0], x[-1])), "numpy")
     f = np.zeros(len(x))
     f[k] = fi(x[k-1], x[k], x[k+1])
     return f
 
-def buildu(ufem):
+phii = sp.lambdify((xsp, xspim1, xspi, xspip1), phi_i, "numpy")
+def buildu(x, ufem):
     N = len(ufem)
     u = np.zeros(N)
-    for n in range(N):
-        u += ufem * phi(x, x, n)
+    for i in range(N):
+        xim1 = x[i - 1] if i > 0 else x[0]+1
+        xip1 = x[i + 1] if i < N - 1 else x[-1]-1
+        xi = x[i]
+        u += ufem[i] * phii(x, xim1, xi, xip1)
     return u
 
 print("Building matrices...")
@@ -138,56 +112,50 @@ K = buildMat(x, dphi_i, dphi_j)
 
 Minv = np.linalg.pinv(M)
 
-plt.figure()
-plt.imshow(K)
-plt.title("K")
-plt.figure()
-plt.imshow(M)
-plt.title("M")
-plt.figure()
-plt.imshow(Minv)
-plt.title("Minv")
-plt.show(block=True)
+# plt.figure()
+# plt.imshow(K)
+# plt.title("K")
+# plt.figure()
+# plt.imshow(M)
+# plt.title("M")
+# plt.figure()
+# plt.imshow(Minv)
+# plt.title("Minv")
+# plt.show(block=True)
 
+f = buildf(x, Nx//2)
 
 #%%
 ufd_0 = np.zeros(Nx)
 ufd_1 = np.zeros(Nx)
 ufd_2 = np.zeros(Nx)
 
+ufem_0 = np.zeros(Nx)
+ufem_1 = np.zeros(Nx)
+ufem_2 = np.zeros(Nx)
+
 lap = np.zeros(Nx)
-fig, ax = plt.subplots()
-line, = ax.plot(x, ufd_0)
-ymm = 10
-ax.set_ylim(-ymm, ymm)
+fig, ax = plt.subplots(2, 1)
+line0, = ax[0].plot(x, ufd_0)
+line1, = ax[1].plot(x, ufd_0)
+ymm = 2e-16
+ax[0].set_ylim(-ymm, ymm)
+ax[0].set_xlabel(None)
+ax[1].set_ylim(-ymm, ymm)
+
+plt.tight_layout()
 
 for nt in range(Nt):
     ufd_1, ufd_2 = ufd_0, ufd_1
     lap[1:-1] = ufd_1[:-2] - 2 * ufd_1[1:-1] + ufd_1[2:]
     ufd_0 = 2 * ufd_1 - ufd_2 + cc * lap
-    ufd_0[1] += s[nt]
-    line.set_ydata(ufd_0)
-    ax.set_title(f"{nt/Nt:.2f}")
-    plt.pause(0.0001)
+    ufd_0[Nx//2] += dt**2 * s[nt]
+    line0.set_ydata(ufd_0)
+    ax[0].set_title(f"FD {nt/Nt:.2f}")
 
-#%%
-f = buildf(x, 1)
-ufem_0 = np.zeros(Nx)
-ufem_1 = np.zeros(Nx)
-ufem_2 = np.zeros(Nx)
-
-fig, ax = plt.subplots()
-line, = ax.plot(x, ufem_0)
-ymm = 1e-16
-ax.set_ylim(-ymm, ymm)
-
-for nt in range(Nt):
     ufem_1, ufem_2 = ufem_0, ufem_1
     ufem_0 = 2 * ufem_1 - ufem_2 + dt**2 * Minv.T @ (f*s[nt] - c[0]**2 * K.T @ ufem_1)
-    # ufem_0 = 2 * ufem_1 - ufem_2 + dt**2 * c[0] ** 2 * Minv.T @ (f * s[nt] - ufem_1)
-    # ufem_0 = 2 * ufem_1 - ufem_2 + dt**2 * ( - c[0] ** 2 * K.T @ ufem_1 / dx)
-    # ufem_0[Nx//2] += s[nt]
-    # line.set_ydata(buildu(ufem_0))
-    line.set_ydata(ufem_0)
-    ax.set_title(f"{nt/Nt:.2f}")
+    line1.set_ydata(buildu(x, ufem_0))
+    # line1.set_ydata(ufem_0)
+    ax[1].set_title(f"FEM {nt/Nt:.2f}")
     plt.pause(0.0001)
